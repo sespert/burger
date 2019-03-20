882b534f05a8ca5ec0a9779bf6d8d2830a895b74
sespertg <sespert@gmail.com>
2019-03-20 10:34:43 -0400

    cleardb

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,20 @@ var mysql = require("mysql");
 //To read and set any environment variables with the dotenv package
 require("dotenv").config();
 
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: process.env.PASSW,
+//     database: "burgers_db"
+//   });
+
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
+    host: "us-cdbr-iron-east-03.cleardb.net",
+    user: "b735928627ace3",
     password: process.env.PASSW,
-    database: "burgers_db"
-  });
+    daatabase: "heroku_6955d38c9d6a5ef"
+})
 
 //Make the connection
 connection.connect(function(err) {
