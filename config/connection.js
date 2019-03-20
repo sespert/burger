d5c4921c8feaaed64ba842a504797c1ab05853f7
sespertg <sespert@gmail.com>
2019-03-20 10:42:39 -0400

    cleardb

--- config/connection.js
+++ config/connection.js
@@ -14,8 +14,8 @@ require("dotenv").config();
 var connection = mysql.createConnection({
     host: "us-cdbr-iron-east-03.cleardb.net",
     user: "b735928627ace3",
-    password: process.env.PASSW,
-    daatabase: "heroku_6955d38c9d6a5ef"
+    password: "eeba5292",
+    database: "heroku_6955d38c9d6a5ef"
 })
 
 //Make the connection
