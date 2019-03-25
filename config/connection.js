931c6b8579531cf055149c9e345d9ed6e645189a
sespertg <sespert@gmail.com>
2019-03-25 13:58:12 -0400

    Change DB so the heroku app doesnt crash

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,16 @@ var mysql = require("mysql");
 //To read and set any environment variables with the dotenv package
 require("dotenv").config();
 
-//Settings to connect to clearDB so the app can be deployed to heroku
-var connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-03.cleardb.net",
-  user: "b735928627ace3",
-  password: "eeba5292",
-  database: "heroku_6955d38c9d6a5ef"
-})
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.PASSW,
+    database: "burgers_db"
+  })
+}
 
 
 
